6769a2d8f9ec6bdeab8ca22f0b23b2f746a2f52b
jeechian <jeechian17@.com>
2025-03-20 01:10:05 +0800

    done

--- app/components/Ending.jsx
+++ app/components/Ending.jsx
@@ -1,26 +1,35 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 
 const Ending = () => {
     return (
         <div className=" bg-slate-950 text-white text-center pt-10">
             
-            <h2  className="flex justify-center w-full mt-0 text-2xl text-yellow-500">
+            <motion.h2  
+            initial={{ opacity: 0, y: -100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            className="flex justify-center w-full mt-0 text-2xl text-yellow-500">
                 Achivement
-            </h2>
-            <h1 className="flex justify-center w-full mt-0 text-4xl font-bold">
+            </motion.h2>
+            <motion.h1
+            initial={{ opacity: 0, x: 100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            className="flex justify-center w-full mt-0 text-4xl font-bold">
             Alumni Sharing Event Speaker
-            </h1>
-            <p className="space-x-2 text-base sm:text-lg p-4">
+            </motion.h1>
+            <motion.p 
+            initial={{ opacity: 0, x: 100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1.2, ease: "easeOut" }}
+            className="space-x-2 text-base sm:text-lg p-4">
             Invited by Ms. Norayu, Head of Program, to share insights and
             experiences with Certificate in Information Technology students.
-            </p>
-            <h2 className="pt-10">
-                
-            </h2>
-            <h2 className="p-3">
-                
-            </h2>
+            </motion.p>
         </div>
     )
 }
